1b0c859398d5a7bdbf6a2c3cfeb982e25d5e4ff3
Wesley vicente <wesley1535@hotmail.com>
2020-06-11 14:36:07 -0300

    adicionado select com cartões

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,28 +2,60 @@ import React, { useState, useEffect } from "react";
 import "./styles.css";
 
 export default function App() {
-  const [taxa] = useState([
-    4.54,
-    6.01,
-    7.01,
-    8.01,
-    8.99,
-    10.31,
-    11.84,
-    12.94,
-    13.94,
-    14.44,
-    15.74,
-    16.04,
-  ]);
+  const [taxa] = useState({
+    master: [
+      4.54,
+      6.01,
+      7.01,
+      8.01,
+      8.99,
+      10.31,
+      11.84,
+      12.94,
+      13.94,
+      14.44,
+      15.74,
+      16.04,
+    ],
+    visa: [
+      1.54,
+      2.01,
+      3.01,
+      10.01,
+      8.99,
+      10.31,
+      11.84,
+      12.94,
+      13.94,
+      14.44,
+      15.74,
+      16.04,
+    ],
+    elo: [
+      15.54,
+      20.01,
+      30.01,
+      10.01,
+      8.99,
+      10.31,
+      11.84,
+      12.94,
+      13.94,
+      14.44,
+      15.74,
+      16.04,
+    ],
+  });
 
   const [valorCompra, setValorCompra] = useState(0);
   const [valorEntrada, setValorEntrada] = useState(0);
   const [parcelas, setParcelas] = useState([]);
   const [taxaDebito] = useState(2);
   const [debitoTotal, setDebitoTotal] = useState(0);
+  const [cartao, setCartao] = useState(0);
 
   useEffect(() => {
+    console.log(cartao);
     function handleChangeInput() {
       if (!valorCompra) {
         setParcelas([]);
@@ -32,7 +64,8 @@ export default function App() {
       if (isNaN(valorEntrada)) {
         setValorEntrada(0);
       }
-      const parcelas = taxa.map((item, index) => {
+
+      const parcelas = taxa.master.map((item, index) => {
         const valor = valorCompra - valorEntrada;
         const percentual = (item * valor) / 100;
         const valueReturn = valor + percentual;
@@ -90,6 +123,16 @@ export default function App() {
                 value={valorEntrada}
                 onChange={(e) => setValorEntrada(e.target.value)}
               />
+            </div>
+            <div className="group">
+              <label>Cartão:</label>
+              <select name="select" onChange={(e) => setCartao(e.target.value)}>
+                <option value={0}>Master</option>
+                <option value={1} selected>
+                  Visa
+                </option>
+                <option value={2}>Elo</option>
+              </select>
             </div>
           </form>
         </div>
